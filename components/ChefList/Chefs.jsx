995ad537ddb21ac44dd16fa20b0038edcb2ce234
EmreOztrk <104931289+EmreOztrk@users.noreply.github.com>
2022-10-26 20:17:19 +0300

    push

--- components/ChefList/Chefs.jsx
+++ components/ChefList/Chefs.jsx
@@ -9,13 +9,16 @@ const Chefs = () => {
             <div className='absolute'>
                 <Image src={"/images/t2.png"} alt="" width={"360px"} height="400px"/>
             </div>
-            <div className='absolute bottom-0 left-1/4 w-full'>
+            <div className='absolute bottom-10 left-1/4 w-full'>
                 <ul className='flex gap-x-2 text-2xl'>
                     <li className='p-2 bg-white rounded-full grid place-content-center'><FaFacebookF/></li>
                     <li className='p-2 bg-white rounded-full grid place-content-center'><FaTwitter/></li>
                     <li className='p-2 bg-white rounded-full grid place-content-center'><FaLinkedin/></li>
                     <li className='p-2 bg-white rounded-full grid place-content-center'><FaInstagram/></li>
                 </ul>
+            </div>
+            <div>
+                <h1>Ajay S.</h1>
             </div>
         </div>
     </div>
