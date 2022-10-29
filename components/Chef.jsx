87e885ecb0abba57460e2b777020662afdf5013f
EmreOztrk <104931289+EmreOztrk@users.noreply.github.com>
2022-10-29 20:52:58 +0300

    Update Chef.jsx

--- components/Chef.jsx
+++ components/Chef.jsx
@@ -7,8 +7,8 @@ const Chef = () => {
   return (
     <div>
         <div className='relative'>
-            <div className='absolute w-full h-[736px] before:content-[""] before:absolute before:w-full before:h-full before:bg-black before:opacity-90 before:z-20'>
-                <Image src={"/images/team_bg.jpg"} alt="" layout='fill'/>
+            <div className='absolute w-full h-[1700px] sm:h-[736px] before:content-[""] before:absolute before:w-full before:h-full before:bg-black before:opacity-90 before:z-20'>
+                <Image src={"/images/team_bg.jpg"} alt="" layout='fill' objectFit='content'/>
             </div>
             <div className='relative z-40 py-20 flex flex-col'>
                 <div className='flex items-center flex-col gap-y-5'>
@@ -19,7 +19,7 @@ const Chef = () => {
                         <Image src={"/images/heading-dark.png"} alt="" layout='fill'/>
                     </div>
                 </div>
-                <div className='flex container mx-auto gap-x-5 py-20'>
+                <div className='flex container mx-auto gap-x-5 py-20 flex-col gap-y-5 sm:flex-row items-center sm:justify-center flex-wrap'>
                     <Chefs/>
                     <Chefs/>
                     <Chefs/>
